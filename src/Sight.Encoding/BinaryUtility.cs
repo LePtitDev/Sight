848fa939ad3f1544d910d1f9862b565e108f81ff
Arthur Ferre <a.tutur@hotmail.fr>
2024-10-30 15:40:04 +0100

    First implementation of encoding lib

---
+++ src/Sight.Encoding/BinaryUtility.cs
@@ -0,0 +1,153 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Sight.Encoding
+{
+    /// <summary>
+    /// Helpers for binary operations
+    /// </summary>
+    public static class BinaryUtility
+    {
+        #region ReverseEndianness
+
+        /// <summary>
+        /// Reverse big/little bytes
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte ReverseEndianness(byte value) => value;
+
+        /// <summary>
+        /// Reverse big/little bytes
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ushort ReverseEndianness(ushort value)
+        {
+            return (ushort)((value << 8) | (value >> 8));
+        }
+
+        /// <summary>
+        /// Reverse big/little bytes
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static uint ReverseEndianness(uint value)
+        {
+            return (value << 24) | ((value & 0x00FF00u) << 8) | ((value & 0x00FF0000) >> 8) | (value >> 24);
+        }
+
+        /// <summary>
+        /// Reverse big/little bytes
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong ReverseEndianness(ulong value)
+        {
+            return ReverseEndianness((uint)(value >> 32)) | (ulong)ReverseEndianness((uint)(value & 0xFFFFFFFF)) << 32;
+        }
+
+        #endregion
+
+        #region WriteToBuffer
+
+        /// <summary>
+        /// Write bytes into a buffer
+        /// </summary>
+        public static void WriteByteToBuffer(byte value, byte[] buffer, int offset = 0, bool littleEndian = true)
+        {
+            buffer[offset] = value;
+        }
+
+        /// <summary>
+        /// Write bytes into a buffer
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteShortToBuffer(short value, byte[] buffer, int offset = 0, bool littleEndian = true)
+        {
+            WriteUShortToBuffer((ushort)value, buffer, offset, littleEndian);
+        }
+
+        /// <summary>
+        /// Write bytes into a buffer
+        /// </summary>
+        public static void WriteUShortToBuffer(ushort value, byte[] buffer, int offset = 0, bool littleEndian = true)
+        {
+            if (littleEndian)
+            {
+                buffer[offset++] = (byte)(value & 0xFF);
+                buffer[offset] = (byte)(value >> 8);
+            }
+            else
+            {
+                buffer[offset++] = (byte)(value >> 8);
+                buffer[offset] = (byte)(value & 0xFF);
+            }
+        }
+
+        /// <summary>
+        /// Write bytes into a buffer
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteIntToBuffer(int value, byte[] buffer, int offset = 0, bool littleEndian = true)
+        {
+            WriteUIntToBuffer((uint)value, buffer, offset, littleEndian);
+        }
+
+        /// <summary>
+        /// Write bytes into a buffer
+        /// </summary>
+        public static void WriteUIntToBuffer(uint value, byte[] buffer, int offset = 0, bool littleEndian = true)
+        {
+            if (littleEndian)
+            {
+                buffer[offset++] = (byte)(value & 0xFF);
+                buffer[offset++] = (byte)((value >> 8) & 0xFF);
+                buffer[offset++] = (byte)((value >> 16) & 0xFF);
+                buffer[offset] = (byte)(value >> 24);
+            }
+            else
+            {
+                buffer[offset++] = (byte)(value >> 24);
+                buffer[offset++] = (byte)((value >> 16) & 0xFF);
+                buffer[offset++] = (byte)((value >> 8) & 0xFF);
+                buffer[offset] = (byte)(value & 0xFF);
+            }
+        }
+
+        /// <summary>
+        /// Write bytes into a buffer
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteLongToBuffer(long value, byte[] buffer, int offset = 0, bool littleEndian = true)
+        {
+            WriteULongToBuffer((ulong)value, buffer, offset, littleEndian);
+        }
+
+        /// <summary>
+        /// Write bytes into a buffer
+        /// </summary>
+        public static void WriteULongToBuffer(ulong value, byte[] buffer, int offset = 0, bool littleEndian = true)
+        {
+            if (littleEndian)
+            {
+                buffer[offset++] = (byte)(value & 0xFF);
+                buffer[offset++] = (byte)((value >> 8) & 0xFF);
+                buffer[offset++] = (byte)((value >> 16) & 0xFF);
+                buffer[offset++] = (byte)((value >> 24) & 0xFF);
+                buffer[offset++] = (byte)((value >> 32) & 0xFF);
+                buffer[offset++] = (byte)((value >> 40) & 0xFF);
+                buffer[offset++] = (byte)((value >> 48) & 0xFF);
+                buffer[offset] = (byte)((value >> 56) & 0xFF);
+            }
+            else
+            {
+                buffer[offset++] = (byte)((value >> 56) & 0xFF);
+                buffer[offset++] = (byte)((value >> 48) & 0xFF);
+                buffer[offset++] = (byte)((value >> 40) & 0xFF);
+                buffer[offset++] = (byte)((value >> 32) & 0xFF);
+                buffer[offset++] = (byte)((value >> 24) & 0xFF);
+                buffer[offset++] = (byte)((value >> 16) & 0xFF);
+                buffer[offset++] = (byte)((value >> 8) & 0xFF);
+                buffer[offset] = (byte)(value & 0xFF);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ tests/Sight.Encoding.Tests/BinaryUtilityTests.cs
@@ -0,0 +1,124 @@
+﻿namespace Sight.Encoding.Tests
+{
+    public class BinaryUtilityTests
+    {
+        [Test]
+        public void ReverseByte()
+        {
+            Assert.AreEqual(0xA8, BinaryUtility.ReverseEndianness(0xA8));
+        }
+
+        [Test]
+        public void ReverseUShort()
+        {
+            Assert.AreEqual(0xA8C2u, BinaryUtility.ReverseEndianness((ushort)0xC2A8u));
+        }
+
+        [Test]
+        public void ReverseUInt()
+        {
+            Assert.AreEqual(0xB4F5A8C2u, BinaryUtility.ReverseEndianness((uint)0xC2A8F5B4u));
+        }
+
+        [Test]
+        public void ReverseULong()
+        {
+            Assert.AreEqual(0xB45DF5A258C2B1A7u, BinaryUtility.ReverseEndianness((ulong)0xA7B1C258A2F55DB4u));
+        }
+
+        [Test]
+        public void WriteByteToBuffer()
+        {
+            var buffer = new byte[4];
+            BinaryUtility.WriteByteToBuffer(0b0110_0010, buffer);
+
+            Assert.AreEqual(0b0110_0010, buffer[0]);
+            Assert.AreEqual(0, buffer[1]);
+            Assert.AreEqual(0, buffer[2]);
+            Assert.AreEqual(0, buffer[3]);
+        }
+
+        [Test]
+        public void WriteShortToBuffer()
+        {
+            var buffer = new byte[4];
+            var value = (ushort)0b1001_0110__0110_0010;
+            BinaryUtility.WriteShortToBuffer((short)value, buffer);
+
+            Assert.AreEqual(0b0110_0010, buffer[0]);
+            Assert.AreEqual(0b1001_0110, buffer[1]);
+            Assert.AreEqual(0, buffer[2]);
+            Assert.AreEqual(0, buffer[3]);
+        }
+
+        [Test]
+        public void WriteUShortToBuffer()
+        {
+            var buffer = new byte[4];
+            BinaryUtility.WriteUShortToBuffer(0b1001_0110__0110_0010, buffer);
+
+            Assert.AreEqual(0b0110_0010, buffer[0]);
+            Assert.AreEqual(0b1001_0110, buffer[1]);
+            Assert.AreEqual(0, buffer[2]);
+            Assert.AreEqual(0, buffer[3]);
+        }
+
+        [Test]
+        public void WriteIntToBuffer()
+        {
+            var buffer = new byte[4];
+            var value = 0b1010_0101__0100_1110__1001_0110__0110_0010;
+            BinaryUtility.WriteIntToBuffer((int)value, buffer);
+
+            Assert.AreEqual(0b0110_0010, buffer[0]);
+            Assert.AreEqual(0b1001_0110, buffer[1]);
+            Assert.AreEqual(0b0100_1110, buffer[2]);
+            Assert.AreEqual(0b1010_0101, buffer[3]);
+        }
+
+        [Test]
+        public void WriteUIntToBuffer()
+        {
+            var buffer = new byte[4];
+            BinaryUtility.WriteUIntToBuffer(0b1010_0101__0100_1110__1001_0110__0110_0010, buffer);
+
+            Assert.AreEqual(0b0110_0010, buffer[0]);
+            Assert.AreEqual(0b1001_0110, buffer[1]);
+            Assert.AreEqual(0b0100_1110, buffer[2]);
+            Assert.AreEqual(0b1010_0101, buffer[3]);
+        }
+
+        [Test]
+        public void WriteLongToBuffer()
+        {
+            var buffer = new byte[8];
+            var value = 0b1010_0101__0100_1110__1001_0110__0110_0010__0101_1010__1011_0001__0110_1001__1001_1101;
+            BinaryUtility.WriteLongToBuffer((long)value, buffer);
+
+            Assert.AreEqual(0b1001_1101, buffer[0]);
+            Assert.AreEqual(0b0110_1001, buffer[1]);
+            Assert.AreEqual(0b1011_0001, buffer[2]);
+            Assert.AreEqual(0b0101_1010, buffer[3]);
+            Assert.AreEqual(0b0110_0010, buffer[4]);
+            Assert.AreEqual(0b1001_0110, buffer[5]);
+            Assert.AreEqual(0b0100_1110, buffer[6]);
+            Assert.AreEqual(0b1010_0101, buffer[7]);
+        }
+
+        [Test]
+        public void WriteULongToBuffer()
+        {
+            var buffer = new byte[8];
+            BinaryUtility.WriteULongToBuffer(0b1010_0101__0100_1110__1001_0110__0110_0010__0101_1010__1011_0001__0110_1001__1001_1101, buffer);
+
+            Assert.AreEqual(0b1001_1101, buffer[0]);
+            Assert.AreEqual(0b0110_1001, buffer[1]);
+            Assert.AreEqual(0b1011_0001, buffer[2]);
+            Assert.AreEqual(0b0101_1010, buffer[3]);
+            Assert.AreEqual(0b0110_0010, buffer[4]);
+            Assert.AreEqual(0b1001_0110, buffer[5]);
+            Assert.AreEqual(0b0100_1110, buffer[6]);
+            Assert.AreEqual(0b1010_0101, buffer[7]);
+        }
+    }
+}
